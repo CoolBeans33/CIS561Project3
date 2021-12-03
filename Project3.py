7e3a5448083f21c72208cedc6d0fd2d284f9a6af
Kameron Jusseaume <kamjuss12198@gmail.com>
2021-12-02 21:23:27 -0500

    Matrix Functionality
    +Added ability to form a list from the csv
    +got the frequency
    :]

--- Project3.py
+++ Project3.py
@@ -1,13 +1,36 @@
 import numpy as np
 import pandas as pd
+import csv
+import sys
 
 
-concepts = pd.read_csv("concepts.csv", header=None)
+with open('concepts.csv', newline='') as f:
+    reader = csv.reader(f)
+    data = list(reader)
 
-concepts[0] = concepts[0].str.lower()
-num_concepts = concepts.nunique()[0]
-print(concepts.value_counts())
-print(num_concepts)
+print(data)
 
-frequency_matrix = np.zeros((num_concepts, num_concepts))
-print(frequency_matrix)
+# using join() +  split() to
+# perform removal
+for i in range(len(data)):
+    data[i] = ' '.join(data[i]).split()
+print(data)
+
+words = set()
+for sublst in data:
+    words |= set(sublst)
+words = list(words)
+print("Words:\n",words)
+
+result = [[0] * len(words)] * len(words) # zeros matrix N x N
+
+for sublst in data:
+    sublst = list(set(sublst)) # selecting unique words only
+    for i in range(len(sublst)):
+        for j in range(i + 1, len(sublst)):
+            index1 = words.index(sublst[i])
+            index2 = words.index(sublst[j])
+            result[index1][index2] += 1
+            result[index2][index1] += 1
+
+print(result)
